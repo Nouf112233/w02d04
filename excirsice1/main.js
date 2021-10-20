327f116d0e1bbe1c4a78c129fec8a708f42ae349
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-10-20 20:59:12 +0300

    edit

--- excirsice1/main.js
+++ excirsice1/main.js
@@ -1,11 +1,14 @@
 // الموجودة بالكومنت هي ألأكواد التي كتبتها سابقا وغيرتها بسبب مشكلة رز التعديل على عناصر القائمة عندما يتم التعديل تختفي الأزرار 
 //لذلك قمت بوضع إنشاء الأزار وإضافتها لعناصر القائمه في دالة واحدة ويتم مناداتها في كل مره يتم حذف عنصر من القائمة او التعديل عليهاg
+//واصبح عندي مشكلة اخرى وهي انه لايتنفذ الا حدث واحد فقط للبوتنز الحذف والتعديل
 const body =document.querySelector("body");
 const header=document.createElement("h1");
 const list=document.createElement("ul");
 const input1=document.createElement("input");
 const button1=document.createElement("button");
 const toDos=["wake up","eat breakfast","code"];
+const idButton1=[];
+const idButton2=[];
 
 button1.innerHTML="click me";
 button1.className ="btn btn-danger dropdown-toggle text-wight";
@@ -24,6 +27,8 @@ body.append(button1);
 
 const renderList=function(){
     list.innerHTML="";
+    idButton1.splice(0,idButton1.length);
+    idButton2.splice(0,idButton2.length);
     for(let i=0;i<toDos.length ;i++) 
     {
         const liItem=document.createElement("li");
@@ -31,13 +36,15 @@ const renderList=function(){
         document.querySelector("#navList").append(liItem);
         const buttonLi=document.createElement("button");
         buttonLi.id=`buttonli-${i}`
+        idButton1.push(buttonLi.id)
         buttonLi.innerHTML="delete";
         buttonLi.className ="btn btn-danger col ml-7px";
         buttonLi.style.marginLeft="7px";
         liItem.append(buttonLi);
         //buttonLi.addEventListener("click",deleteListItem);
         const buttonLi2=document.createElement("button");
-        buttonLi2.id=`buttonli2-${i}`
+        buttonLi2.id=`buttonli2-${i}`;
+        idButton2.push(buttonLi2.id)
         buttonLi2.innerHTML="update";
         buttonLi2.className ="btn btn-warning col ml-7px text-wight";
         buttonLi2.style.marginLeft="7px";
@@ -60,6 +67,7 @@ const deleteListItem=function(i){
     //const parentBut =document.getElementById(idBut).parentNode;
     
     //const indexParent=toDos.indexOf(parentBut.innerText);
+    
     toDos.splice(i,1);
     renderList();
 
@@ -83,19 +91,23 @@ const updateListItem=function(i){
 
 
 const li=document.querySelectorAll("li");
-//const deleteButton=li.firstElementChild;
-//const updateButton=li.lastElementChild;
+
+//deleteButton.addEventListener("click",()=>{deleteListItem(i)});
+//updateButton.addEventListener("click",()=>{updateListItem(i)});
+//const updateButton=li.lastElementChild();
+
  
- for(let i=0;i<toDos.length;i++)
- {
-   const deleteButton=li[i].firstElementChild;
-    const updateButton=li[i].lastElementChild;
+  for(let i=0;i<idButton1.length;i++)
+  {
+      let b1=idButton1[i];
+      let b2=idButton2[i];
+      document.getElementById(idButton1[i]).addEventListener("click",()=>{deleteListItem(i)});
+      document.getElementById(b2).addEventListener("click",()=>{updateListItem(i)});
+//    updateButton.addEventListener("click",()=>{updateListItem(i)});
 
-   deleteButton.addEventListener("click",()=>{deleteListItem(i)});
-   updateButton.addEventListener("click",()=>{updateListItem(i)});
-
- }
+  }
  button1.addEventListener("click",addToList);
+
 
 
 
