c99dc98b5c44dddeb1cd4e526adef93f0a14b71f
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-10-20 22:23:09 +0300

    edit

--- excirsice1/main.js
+++ excirsice1/main.js
@@ -1,6 +1,6 @@
-// الموجودة بالكومنت هي ألأكواد التي كتبتها سابقا وغيرتها بسبب مشكلة رز التعديل على عناصر القائمة عندما يتم التعديل تختفي الأزرار 
-//لذلك قمت بوضع إنشاء الأزار وإضافتها لعناصر القائمه في دالة واحدة ويتم مناداتها في كل مره يتم حذف عنصر من القائمة او التعديل عليهاg
-//واصبح عندي مشكلة اخرى وهي انه لايتنفذ الا حدث واحد فقط للبوتنز الحذف والتعديل
+
+
+// عندي مشكلة  وهي انه لايتفذ إيفنت للبوتن على العنصر المعمول له ابديت او ادد
 const body =document.querySelector("body");
 const header=document.createElement("h1");
 const list=document.createElement("ul");
@@ -26,10 +26,7 @@ body.append(button1);
 //function
 
 const renderList=function(){
-    list.innerHTML="";
-    idButton1.splice(0,idButton1.length);
-    idButton2.splice(0,idButton2.length);
-    for(let i=0;i<toDos.length ;i++) 
+    for(let i=0;i<toDos.length;i++)
     {
         const liItem=document.createElement("li");
         liItem.innerHTML= toDos[i];
@@ -41,7 +38,6 @@ const renderList=function(){
         buttonLi.className ="btn btn-danger col ml-7px";
         buttonLi.style.marginLeft="7px";
         liItem.append(buttonLi);
-        //buttonLi.addEventListener("click",deleteListItem);
         const buttonLi2=document.createElement("button");
         buttonLi2.id=`buttonli2-${i}`;
         idButton2.push(buttonLi2.id)
@@ -49,7 +45,6 @@ const renderList=function(){
         buttonLi2.className ="btn btn-warning col ml-7px text-wight";
         buttonLi2.style.marginLeft="7px";
         liItem.append(buttonLi2);
-        //buttonLi2.addEventListener("click",updateListItem);
     }
 }
 
@@ -57,32 +52,49 @@ renderList();
 
 const addToList=function(){
     toDos.push(input1.value);
-    renderList();
-    // const newLi=document.createElement("li");
-    // newLi.innerHTML=toDos[toDos.length-1];
-    // document.querySelector("#navList").append(newLi);
+    const newli=document.createElement("li");
+    newli.innerText=(input1.value);
+    list.append(newli);
+    const buttonLi=document.createElement("button");
+    let index=idButton1.length;
+    buttonLi.id=`buttonli-${index}`
+    idButton1.push(buttonLi.id)
+    buttonLi.innerHTML="delete";
+    buttonLi.className ="btn btn-danger col ml-7px";
+    buttonLi.style.marginLeft="7px";
+    newli.append(buttonLi);
+    const buttonLi2=document.createElement("button");
+    buttonLi2.id=`buttonli2-${index}`;
+    idButton2.push(buttonLi2.id)
+    buttonLi2.innerHTML="update";
+    buttonLi2.className ="btn btn-warning col ml-7px text-wight";//المارجن هنا لم يتنفذ لذلك وضعت مارجن ب سي اس اس
+    buttonLi2.style.marginLeft="7px";
+    newli.append(buttonLi2);
+
 }
 const deleteListItem=function(i){
-   // const idBut = e.target.id;
-    //const parentBut =document.getElementById(idBut).parentNode;
-    
-    //const indexParent=toDos.indexOf(parentBut.innerText);
-    
     toDos.splice(i,1);
-    renderList();
-
-
+    li[i].remove();
 }
 
 const updateListItem=function(i){
-    //const idBut2 = e.target.id;
-    //const parentBut2 =document.getElementById(idBut2).parentNode;
     let message= prompt("please enter the text of item list");
-    //const indexParent=toDos.indexOf(parentBut2.innerText);
-    
-    //parentBut2.innerText=message;
     toDos.splice(i,1,message);
-    renderList();
+    li[i].innerText=message;
+    const buttonLi=document.createElement("button");
+    buttonLi.id=`buttonli-${i}`
+    idButton1.push(buttonLi.id)
+    buttonLi.innerHTML="delete";
+    buttonLi.className ="btn btn-danger col ml-7px";
+    buttonLi.style.marginLeft="7px";
+    li[i].append(buttonLi);
+    const buttonLi2=document.createElement("button");
+    buttonLi2.id=`buttonli2-${i}`;
+    idButton2.push(buttonLi2.id)
+    buttonLi2.innerHTML="update";
+    buttonLi2.className ="btn btn-warning col ml-7px text-wight";
+    buttonLi2.style.marginLeft="7px";
+    li[i].append(buttonLi2);
 
 }
 
@@ -92,48 +104,16 @@ const updateListItem=function(i){
 
 const li=document.querySelectorAll("li");
 
-//deleteButton.addEventListener("click",()=>{deleteListItem(i)});
-//updateButton.addEventListener("click",()=>{updateListItem(i)});
-//const updateButton=li.lastElementChild();
-
- 
   for(let i=0;i<idButton1.length;i++)
   {
+
       let b1=idButton1[i];
       let b2=idButton2[i];
-      document.getElementById(idButton1[i]).addEventListener("click",()=>{deleteListItem(i)});
+      document.getElementById(b1).addEventListener("click",()=>{deleteListItem(i)});
       document.getElementById(b2).addEventListener("click",()=>{updateListItem(i)});
-//    updateButton.addEventListener("click",()=>{updateListItem(i)});
 
   }
  button1.addEventListener("click",addToList);
-
-
-
-
-// const actionList=()=>{
-//     for(let i=0;i<toDos.length;i++)
-// {
-//     // const buttonLi=document.createElement("button");
-//     // buttonLi.id=`buttonli-${i}`
-//     // buttonLi.innerHTML="delete";
-//     // buttonLi.className ="btn btn-danger col ml-7px";
-//     // buttonLi.style.marginLeft="7px";
-//     // li[i].append(buttonLi);
-//     // buttonLi.addEventListener("click",deleteListItem);
-// }
-
-// for(let i=0;i<toDos.length;i++)
-// {
-//     // const buttonLi2=document.createElement("button");
-//     // buttonLi2.id=`buttonli2-${i}`
-//     // buttonLi2.innerHTML="update";
-//     // buttonLi2.className ="btn btn-warning col ml-7px text-wight";
-//     // buttonLi2.style.marginLeft="7px";
-//     // li[i].append(buttonLi2);
-//     // buttonLi2.addEventListener("click",updateListItem);
-// }
-// }
 
 
 //style
