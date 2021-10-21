f84edb2958d2372c64cf896dd635b60931b4cedc
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-10-21 21:17:13 +0300

    final edit

--- excirsice1/main.js
+++ excirsice1/main.js
@@ -6,10 +6,11 @@ const header=document.createElement("h1");
 const list=document.createElement("ul");
 const input1=document.createElement("input");
 const button1=document.createElement("button");
+const li=document.querySelectorAll("li");
 const toDos=["wake up","eat breakfast","code"];
 const idText=[];
-const idButton1=[];
-const idButton2=[];
+//const idButton1=[];
+//const idButton2=[];
 
 button1.innerHTML="click me";
 button1.className ="btn btn-danger dropdown-toggle text-wight";
@@ -28,24 +29,27 @@ body.append(button1);
 
 const deleteListItem=function(i){
     toDos.splice(i,1);
-    idText.splice(i,1);
-    idButton1.splice(i,1);
-    idButton2.splice(i,1);
-    li[i].remove();
+    renderList();
 }
 
-const updateListItem=function(i){
-    
+const updateListItem=function(i){ 
     let message= prompt("please enter the text of item list");
     toDos.splice(i,1,message);
-    let id =idText[i];
-    const text=document.getElementById(id);
-    text.innerHTML=message;
-
+    renderList();
 }
 
-const renderList=function(i){
-        
+const addToList=function(){
+    toDos.push(input1.value);
+    renderList();
+ }
+
+const renderList=function(){
+    list.innerHTML="";
+    for(let i=0;i<toDos.length;i++)
+    {
+    
+    
+        const li=document.querySelectorAll("li");
         const liItem=document.createElement("li");
         const text=document.createElement("h4");
         const buttonLi=document.createElement("button");
@@ -56,12 +60,10 @@ const renderList=function(i){
         buttonLi2.innerHTML="update";
 
         text.id=`text-${i}`
-        buttonLi.id=`buttonli-${i}`;
-        buttonLi2.id=`buttonli2-${i}`;
+        
        
         idText.push(text.id)
-        idButton1.push(buttonLi.id);
-        idButton2.push(buttonLi2.id);
+        
         
         buttonLi.className ="btn btn-danger col ml-7px";
         buttonLi.style.marginLeft="7px";
@@ -72,37 +74,17 @@ const renderList=function(i){
         liItem.append(text);
         liItem.append(buttonLi);
         liItem.append(buttonLi2); 
+    
         
         buttonLi.addEventListener("click",()=>{deleteListItem(i)});
         buttonLi2.addEventListener("click",()=>{updateListItem(i)});
     }
-
-for(let i=0;i<toDos.length;i++)
-    {
-        renderList(i);
     }
+ 
+renderList();
+    
 
-
-
-const addToList=function(){
-    toDos.push(input1.value);
-    let i=toDos.length-1;
-    renderList(i);
-
- }
-
-const li=document.querySelectorAll("li");
-
-//   for(let i=0;i<toDos.length;i++)
-//   {
-//       renderList(i);
-//       let b1=idButton1[i];
-//       let b2=idButton2[i];
-//       document.getElementById(b1).addEventListener("click",()=>{deleteListItem(i)});
-//       document.getElementById(b2).addEventListener("click",()=>{updateListItem(i)});
-
-//   }
- button1.addEventListener("click",addToList);
+button1.addEventListener("click",addToList);
 
 
 //style
